f70c76b7adb74e8b9356a9b3970816e3b45e864f
Paremir <paremir@mail.ru>
2022-03-16 12:52:54 +0200

    fixed and adden in view foreach property

--- lab4/src/app/app.component.ts
+++ lab4/src/app/app.component.ts
@@ -8,20 +8,18 @@ import {Interface2} from "../module/interface2";
 })
 export class AppComponent implements OnInit{
   title = 'lab4';
-
+  myobj = object;
   ngOnInit(): void {
-    console.log(object._1field1)
-    object._1field1 = 'd';
-    console.log(object._1field1)
+    console.log(object)
   }
 
 }
  export  const object:Interface2 = new class implements Interface2 {
-  _1field1: any = '_1field1';
-  _1field2: any = '_1field2';
-  _1field3: any = '_1field3';
-  _1field4: any = '_1field4';
-  _1field5: any = '_1field5';
-  _2field1: any = '_2field1';
-  _2field2: any = '_2field2';
+  _1field1: any = '_1field1 Value';
+  _1field2: any = '_1field2 Value';
+  _1field3: any = '_1field3 Value';
+  _1field4: any = '_1field4 Value';
+  _1field5: any = '_1field5 Value';
+  _2field1: any = '_2field1 Value';
+  _2field2: any = '_2field2 Value';
 }
