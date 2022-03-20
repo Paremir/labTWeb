65ef71f045fff3d8962e391bc28bbe6b18b6540f
Paremir <paremir@mail.ru>
2022-03-20 16:31:53 +0200

    ssda

--- lab4/src/app/app.component.ts
+++ lab4/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Interface2} from "../module/interface2";
+import {Interface} from "readline";
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit{
 
 }
  export  const object:Interface2 = new class implements Interface2 {
-  _1field1: any = '_1field1 Value';
+  _1field1: any = '_1field1 ';
   _1field2: any = '_1field2 Value';
   _1field3: any = '_1field3 Value';
   _1field4: any = '_1field4 Value';
@@ -23,3 +24,25 @@ export class AppComponent implements OnInit{
   _2field1: any = '_2field1 Value';
   _2field2: any = '_2field2 Value';
 }
+
+class asdasdasd implements  Interface2{
+  _1field1: any;
+  _1field2: any;
+  _1field3: any;
+  _1field4: any;
+  _1field5: any;
+  _2field1: any;
+  _2field2: any;
+
+  constructor() {
+    this._1field1 = '_1field1 ';
+    this._1field2 = '_1field2 Value';
+    this._1field3 = '_1field3 Value';
+    this._1field4 = '_1field4 Value';
+    this._1field5 = '_1field5 Value';
+    this._2field1 = '_2field1 Value';
+    this._2field2 = '_2field2 Value';
+  }
+}
+
+export const o: asdasdasd = new asdasdasd();
